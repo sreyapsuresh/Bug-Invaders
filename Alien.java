11240b8f18655b5486a01243e07303ba0c336246
sreyapsuresh <sreyapsuresh@gmail.com>
2022-07-11 10:35:45 -0500

    Bug fixes! Game now properly works
    Just need to edit assets and change up variable names (Maybe add comments too?? We'll see!)

--- Alien.java
+++ Alien.java
@@ -1,32 +1,39 @@
-public class Alien extends Character 
+import java.io.File;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
+public class Alien extends SpaceCharacter
 {
-    boolean moveRight;
-    boolean moveLeft;
-    boolean isVisible; // when hit, they are no longer visible
-
-    public Alien(int x, int y, int s)
+     boolean isVis;
+     
+    public Alien()
     {
-        super(x,y,s);
-
-        moveLeft = false;
-        moveRight = true; // starts the game off by moving
-        isVisible = true;
+        super();
     }
+
+    public Alien(int x, int y, int w, int h, int s, String u)
+    {
+        super(x, y, w, h, s, u);
+        isVis=true;
+        moveRight=true;
+    }
+
+    public void mover(){
+
+    }
+    public  void move(int direction){
+        if(moveLeft==true)
+            setX(getX()-getSpeed());
+            
+        if(moveRight==true)
+            setX(getX()+getSpeed());
+            
+    }
+    public void draw(Graphics window){
+        window.drawImage(getImage(),getX(),getY(),getWidth(),getHeight(),null);
+    }
+
 }
-
-public void mover(){
-
-}
-public void move(int direction){
-    if(moveLeft==true)
-        setX(getX()-getSpeed());
-
-    if(moveRight==true)
-        setX(getX()+getSpeed());
-}
-
-public void draw(Graphics window){
-    window.drawImage(getImage(), getX(), getY(), getWidth(), getHeight(), null)
-}
-
-
--- Ship.java
+++ Ship.java
@@ -5,38 +5,32 @@ import java.awt.Graphics;
 import java.awt.Image;
 import javax.imageio.ImageIO;
 
-public class Shot extends SpaceCharacter
+public class Ship extends SpaceCharacter
 {
 
-    public Shot()
+    public Ship()
     {
         super();
     }
 
-    boolean goUp;
-
-    public Shot(int x, int y, int w, int h, int s, String u)
+    public Ship(int x, int y, int w, int h, int s, String u)
     {
-        super(x, y, w, h, s, "shot.png");
-        setY(600);
-        goUp = false;
-    }
-
-    public  void move(int d){
-
-        if(getY()<0){
-            goUp=false;
-            setY(600);
-        }
-
-        if(goUp)
-            setY(getY()-getSpeed());
+        super(x, y, w, h, s, "player.png");
     }
 
     public void draw(Graphics window){
         window.drawImage(getImage(),getX(),getY(),getWidth(),getHeight(),null);
     }
 
+    public  void move(int d){
+        if(moveLeft)
+            setX(getX()-getSpeed());
+
+        if(moveRight){
+            setX(getX()+getSpeed());
+        }
+
+    }
 
     public void setLeftRight(int d){
         if(d==37){
