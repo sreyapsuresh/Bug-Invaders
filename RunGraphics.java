ffe623a1d68256ef6dfef685aee8160af7304196
sreyapsuresh <sreyapsuresh@gmail.com>
2022-07-14 01:55:43 -0500

    Cleaned up code

--- RunGraphics.java
+++ RunGraphics.java
@@ -17,7 +17,6 @@ import java.awt.FontMetrics;
 import java.util.*;
 import javax.swing.Timer;
 import java.awt.event.*;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -25,93 +24,111 @@ import java.io.InputStream;
 import java.util.Scanner;
 import javax.sound.sampled.*;
 
-public class RunGraphics {
+/*
+*  GAME CONTROLS:
+* 
+*  Player 1 controls Agent Zero, and moves using the A & D keys. Use W to shoot.
+*  Player 2 controls Agent One, and moves using the <- & -> keys. Use Spacebar to shoot.
+* 
+*/
 
+public class RunGraphics 
+{
     private JFrame frame;
     int fW = 600;
     int fH = 600;
 
-    // The method to set up
-    public RunGraphics() {
-
+    // Window set-up
+    public RunGraphics() 
+    {
         frame = new JFrame("Bug Invaders");
         frame.setSize(fW, fH);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // the event that triggers the end of the program
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setPreferredSize(frame.getSize());
-        frame.add(new showGraphics(frame.getSize())); // Setting up the DrawBars public class function (getting bars and
-        // putting it in this frame)
+        frame.add(new showGraphics(frame.getSize())); // Setting up the DrawBars public class function (getting bars and putting it in this frame)
         frame.pack();
         frame.setVisible(true); 
     }
 
-    // The main method
+    // Main method
     public static void main(String[] args) 
     {
         new RunGraphics();
 
-        // music player
+        // Music player
         MusicPlayer.RunMusic("bugDestroyers.wav");
-
     }
-    public static class showGraphics extends JPanel implements Runnable, MouseListener, KeyListener {
-
+    public static class showGraphics extends JPanel implements Runnable, MouseListener, KeyListener 
+    {
         private Thread animator;
 
+        // Initialization
         int xAxis;
         int yAxis;
+        Boolean gameOn = false;
 
-        Ship s;
-        Ship agentTwo;
+        String emotionZero = "Neutral"; // Expression of Agent Zero
+        String emotionOne = "Neutral"; // Agent One
+        
+        // Ship entities
+        Ship agentZero;
+        Ship agentOne;
 
+        // Shot entities
+        Shot zeroShot;
+        Shot oneShot;
+        
+        // Aliens
         Alien[][] a = new Alien[3][10];
 
-        Shot sh;
-        Shot twoShot;
+        // Scoring
+        int zeroPts;
+        int onePts;
 
-        Boolean gameOn = false;
-        int points;
-        String emotion = "Neutral";
-
-        public showGraphics(Dimension dimension) {
-            
+        public showGraphics(Dimension dimension) 
+        {
             gameSetup();
 
             setSize(dimension);
             setPreferredSize(dimension);
             addMouseListener(this);
-            addKeyListener ( this ) ; 
-            //addActionListener(this);
+            addKeyListener(this) ; 
             setFocusable(true);
-            if (animator == null) {
+            if (animator == null) 
+            {
                 animator = new Thread(this);
                 animator.start();
             }
-
             setDoubleBuffered(true);
-
         }
 
-        public void gameSetup() // sets up the game
+        public void gameSetup() // Sets up entities
         {
+            // Resets variables 
             yAxis = 30;
             xAxis = 30;
-            points = 0;
-
-            a = new Alien[3][10];
-
+            zeroPts = 0;
+            onePts = 0;
             gameOn = false;
             
-            s = new Ship(100,500,57,35,5,"player.png");
-            agentTwo = new Ship(300,500,57,35,5,"player.png");
+            // Ships
+            agentOne = new Ship(100,500,57,35,5,"player.png");                                                             // **CHANGE SPRITE**
+            agentZero = new Ship(300,500,57,35,5,"player.png");
             
-            sh = new Shot(100,500,5,20,15,"shot.png");
-            twoShot = new Shot(300,500,5,20,15,"shot.png");
+            // Shots
+            oneShot = new Shot(100,500,5,20,15,"shot.png");
+            zeroShot = new Shot(300,500,5,20,15,"shot.png");                                                               // **CHANGE SPRITE**
+
+            // Alien
+            a = new Alien[3][10];
 
             int x = 10;
             int y = 70;
 
-            for(int r = 0; r<a.length; r++){
-                for (int c = 0; c<a[0].length; c++){
+            for(int r = 0; r<a.length; r++)
+            {
+                for (int c = 0; c<a[0].length; c++)
+                {
                     a[r][c] = new Alien(x,y,30,20,5,"alien.png");
                     x += 35;
                 }
@@ -120,153 +137,199 @@ public class RunGraphics {
             }      
         }
 
-        public void paintComponent(Graphics g) {
+        public void paintComponent(Graphics g) 
+        {
+            // Different "layers" of the game
+            Graphics2D gBackground = (Graphics2D) g; // Background color
+            Graphics2D gScore = (Graphics2D) g; // Score text
+            Graphics2D gSprite = (Graphics2D) g; // Sprites
+            Graphics2D gMenu = (Graphics2D) g; // Menu background
+            Graphics2D gFont = (Graphics2D) g; // Game font
 
-            Graphics2D g2 = (Graphics2D) g;// g2 is the graphics object that we need to use
-            Graphics2D g3 = (Graphics2D) g;
-            Graphics2D g4 = (Graphics2D) g;
-
-            // to draw things to the screen
+            // Needed to draw things
             Dimension d = getSize();
 
-            // create a background
-            g2.setColor(Color.black);
-            g2.fillRect(0, 0, d.width, d.height);
+            // Draws background
+            gBackground.setColor(Color.black);
+            gBackground.fillRect(0, 0, d.width, d.height);
 
+            // If game has started, entities can start moving
             if (gameOn == true)
             {
                 moveAlien();
-                s.move(0);
-                sh.move(0);
 
-                agentTwo.move(0);
-                twoShot.move(0);
+                agentZero.move(0);
+                zeroShot.move(0);
 
-                oneSprite(g);
+                agentOne.move(0);
+                oneShot.move(0);
 
-                updateScore(g, g3);
-            } else
+                oneSprite(gSprite);
+                updateScore(g, gScore);
+            } 
+            else
             {
-                startScreen(g, g2);
+                startScreen(g, gMenu);
             }
 
-            sh.draw(g2);
-            s.draw(g2);
+            // Draw sprites
+            zeroShot.draw(gSprite);
+            agentZero.draw(gSprite);
 
-            twoShot.draw(g4);
-            agentTwo.draw(g4);
+            oneShot.draw(gSprite);
+            agentOne.draw(gSprite);
 
             hitDetect();
 
-            for(int r = 0; r<a.length; r++){
-                for (int c = 0; c<a[0].length; c++){
-                    if(a[r][c].isVis)
-                        a[r][c].draw(g2);
+            for(int r = 0; r<a.length; r++)
+            {
+                for (int c = 0; c<a[0].length; c++)
+                {
+                    if(a[r][c].isVis) 
+                    {
+                        a[r][c].draw(gSprite);
+                    }
                 }
             }  
-            
-
-        } // end of paintcomponent
-
-        public void oneSprite(Graphics g) {
-            Toolkit t=Toolkit.getDefaultToolkit();  
-            Image i = t.getImage("one"+ emotion + ".png");  
-            g.drawImage(i, 0, 0,this);
         }
 
+        // Agent Zero Sprite
+        public void zeroSprite(Graphics g)
+        {
+            Toolkit t = Toolkit.getDefaultToolkit();
+            Image zeroSpr = t.getImage("zero"+ emotionZero + ".png");
+            g.drawImage(zeroSpr, 500, 0,this);
+        }
+
+        // Agent One Sprite
+        public void oneSprite(Graphics g) 
+        {
+            Toolkit t = Toolkit.getDefaultToolkit();  
+            Image oneSpr = t.getImage("one"+ emotionOne + ".png");  
+            g.drawImage(oneSpr, 0, 0,this);
+        }
+
+        // Menu screen
         public void startScreen(Graphics g, Graphics2D g2) {
             g2.setColor(Color.white);
             g.drawString("Press S to start", 10, 200);
         }
 
-        public void updateScore(Graphics g, Graphics2D g3) {
-            g3.setColor(Color.white);
-            g.drawString("Score: " + points, 500, 25);
+        // Update score
+        public void updateScore(Graphics g, Graphics2D g2) {
+            g2.setColor(Color.white);
+            g.drawString("P1 Score: " + zeroPts, 500, 25);
+            g.drawString("P2 Score: " + onePts, 500, 75);
         }
 
-        public void hitDetect(){
-
-            for(int r = 0; r<a.length; r++){
-                for (int c = 0; c<a[0].length; c++){
-
-                    // if ( a[r][c].isVisible() ) {//&& shot.isVisible()
-                    if (a[r][c].isVis == true && sh.getX() + sh.getWidth() >= a[r][c].getX() && 
-                    sh.getX() <= a[r][c].getX() + a[r][c].getWidth() && 
-                    sh.getY() + sh.getHeight() >= (a[r][c].getY()) && 
-                    sh.getY() <= a[r][c].getY() + a[r][c].getHeight()) {
-
-                            a[r][c].isVis=false; 
-                            sh.x = -30;
-                            points++;
-                            emotion = "Smile"; 
-                            updateScore(null, null);
-                    } 
-                    if (a[r][c].isVis == true && twoShot.getX() + twoShot.getWidth() >= a[r][c].getX() && 
-                    twoShot.getX() <= a[r][c].getX() + a[r][c].getWidth() && 
-                    twoShot.getY() + twoShot.getHeight() >= (a[r][c].getY()) && 
-                    twoShot.getY() <= a[r][c].getY() + a[r][c].getHeight()) 
+        // Hit detection
+        public void hitDetect()
+        {
+            for(int r = 0; r<a.length; r++)
+            {
+                for (int c = 0; c<a[0].length; c++)
+                {
+                    if (a[r][c].isVis == true && zeroShot.getX() + zeroShot.getWidth() >= a[r][c].getX() && 
+                    zeroShot.getX() <= a[r][c].getX() + a[r][c].getWidth() && 
+                    zeroShot.getY() + zeroShot.getHeight() >= (a[r][c].getY()) && 
+                    zeroShot.getY() <= a[r][c].getY() + a[r][c].getHeight()) 
                     {
                         a[r][c].isVis=false; 
-                            twoShot.x = -30;
+                        
+                        zeroShot.x = -30;
+                        zeroPts++;
+                        emotionZero = "Smile";
+
+                        updateScore(null, null);
                     }
-                    //}
 
-                }}
+                    if (a[r][c].isVis == true && 
+                    oneShot.getX() + oneShot.getWidth() >= a[r][c].getX() && 
+                    oneShot.getX() <= a[r][c].getX() + a[r][c].getWidth() && 
+                    oneShot.getY() + oneShot.getHeight() >= (a[r][c].getY()) && 
+                    oneShot.getY() <= a[r][c].getY() + a[r][c].getHeight()) 
+                    {
+                        a[r][c].isVis=false; 
 
+                        oneShot.x = -30;
+                        onePts++;
+                        emotionOne = "Smile"; 
+
+                        updateScore(null, null);
+                    } 
+                }
+            }
         }
 
-        public void moveAlien(){
-            for(int r = 0; r<a.length; r++){
-                for (int c = 0; c<a[0].length; c++){
+        public void moveAlien()
+        {
+            for(int r = 0; r<a.length; r++)
+            {
+                for (int c = 0; c<a[0].length; c++)
+                {
                     if(a[r][c].moveLeft)
+                    {
                         a[r][c].setX(a[r][c].getX()-a[r][c].getSpeed());
+                    }
 
-                    if(a[r][c].moveRight){
+                    if(a[r][c].moveRight)
+                    {
                         a[r][c].setX(a[r][c].getX()+a[r][c].getSpeed());
                     }
-                }}
-            //check if we need to switch directions
-            for(int r = 0; r<a.length; r++){
-                for (int c = 0; c<a[0].length; c++){
+                }
+            }
 
-                    if(a[r][c].getX()>600){
+            // Check if we need to switch directions
+            for(int r = 0; r<a.length; r++)
+            {
+                for (int c = 0; c<a[0].length; c++)
+                {
+                    if(a[r][c].getX()>600)
+                    {
                         moveLeftRight(1);
                         break;
                     }
 
-                    if(a[r][c].getX()<0){
+                    if(a[r][c].getX()<0)
+                    {
                         moveLeftRight(2);
                         break;
                     }
-                }}
-
+                }
+            }
         }
 
-        public void moveLeftRight(int d){
-            for(int r = 0; r<a.length; r++){
-                for (int c = 0; c<a[0].length; c++){
-                    if(d==1){
+        public void moveLeftRight(int d)
+        {
+            for(int r = 0; r<a.length; r++)
+            {
+                for (int c = 0; c<a[0].length; c++)
+                {
+                    if(d==1)
+                    {
                         a[r][c].moveLeft=true;
                         a[r][c].moveRight=false;
-                    }else{
+                    }
+                    else
+                    {
                         a[r][c].moveLeft=false;
                         a[r][c].moveRight=true;
                     }
 
                     a[r][c].setY(a[r][c].getY()+10);
 
-                    if(a[r][c].getY() > 500) // ships position
+                    if(a[r][c].getY() > 500) // The bottom of the screen
                     {
                         gameOn = false;
                     }
 
-                }}
+                }
+            }
         }
 
         public void mousePressed(MouseEvent e) {
             int x = e.getX();
             int y = e.getY();
-
         }
 
         public void mouseReleased(MouseEvent e) {
@@ -286,55 +349,59 @@ public class RunGraphics {
         }  
 
         public void keyPressed ( KeyEvent e){  
-            // System.out.println("Key: " + e.getKeyCode());
             int k = e.getKeyCode();
 
-            if(k==83)
+            agentZero.setLeftRight(k,37, 39); // <- and ->
+            agentOne.setLeftRight(k, 65, 68); // A and D
+
+            if(k == 83) // S
             {
                 gameSetup();
                 gameOn = true;
             }
 
-            s.setLeftRight(k,37, 39);
-            agentTwo.setLeftRight(k, 65, 68);
-
-            if(k==32)  {
-                sh.goUp=true;
-                sh.setX(s.getX() + (s.getWidth()/2));
-                sh.setY(s.getY() );
-            }
-
-            if(k==86)
+            if(k == 32) // Spacebar
             {
-                twoShot.goUp=true;
-                twoShot.setX(agentTwo.getX() + (agentTwo.getWidth()/2));
-                twoShot.setY(agentTwo.getY() );
+                zeroShot.goUp=true;
+                zeroShot.setX(agentZero.getX() + (agentZero.getWidth()/2));
+                zeroShot.setY(agentZero.getY() );
+            }
+            
+            if(k == 86) // V
+            {
+                oneShot.goUp=true;
+                oneShot.setX(agentOne.getX() + (agentOne.getWidth()/2));
+                oneShot.setY(agentOne.getY() );
             }
         }  
 
         public void keyReleased ( KeyEvent e ){  
             int k = e.getKeyCode();
-            s.stop();
-            agentTwo.stop();
 
-        }  
-        public void run() {
+            agentZero.stop();
+            agentOne.stop();
+        } 
+
+        public void run() 
+        {
             long beforeTime, timeDiff, sleep;
             beforeTime = System.currentTimeMillis();
             int animationDelay = 50;
             long time = System.currentTimeMillis();
-            while (true) {// infinite loop
-                // spriteManager.update();
+
+            while (true) // Infinite loop
+            {
                 repaint();
-                try {
+                try 
+                {
                     time += animationDelay;
                     Thread.sleep(Math.max(0, time - System.currentTimeMillis()));
-                } catch (InterruptedException e) {
+                } 
+                catch (InterruptedException e) 
+                {
                     System.out.println(e);
-                } // end catch
-            } // end while loop
-        }// end of run
-
+                }   // End catch
+            }   // End while loop
+        }   // End of run
     }
-
 }
